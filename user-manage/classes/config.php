eb2e3c8fe039f94fb3abf37286486e536e4e5629
lehoaiyenthy <ifrc.yenthy@gmail.com>
2017-05-26 15:43:10 +0700

    thy

---
+++ user-manage/classes/config.php
@@ -0,0 +1,17 @@
+<?php
+
+////////////////////
+// Important ! These must be filled in correctly.
+// Database details are required to use this script.
+
+/*$host = "27.0.14.49"; // If you don't know what your host is, it's safe to leave it localhost
+$dbName = "simulati_data"; // Database name
+$dbUser = "simulati_user1"; // Username
+$dbPass = "@@simulation11"; // Password*/
+
+$host = "localhost"; // If you don't know what your host is, it's safe to leave it localhost
+$dbName = "commo"; // Database name
+$dbUser = "root"; // Username
+$dbPass = ""; // Password
+
+?>
