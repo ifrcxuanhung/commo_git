c61f3777e43a41c83d1fb641ed018efd61884485
ifrcxuanhung <Xuanhung2211>
2017-05-30 09:30:43 +0700

    Merge branch 'master' of commo.ifrc.vn:~/git/commo
    # Conflicts:
    #	_/application/config/database.php

--- assets/apps/welcome/views/customise.js
+++ assets/apps/welcome/views/customise.js
@@ -18,146 +18,115 @@ define([
 
 			 var last_new;
 			 var time_new;
-			 function getvalueCHART(){
-				//console.log(code_chart);
-				return $.ajax({
-					url: $base_url + "ajax/getSpectIntraday_customise1",
-					type: "POST",
+                   function getvalueCHART_1(){
+                       //console.log(code_chart);
+                       return $.ajax({
+                           url: $base_url + "ajax/getSpectIntraday_customise1",
+                           type: "POST",
 
-					async: false
-				});
-			}
-							
-			var data1 = jQuery.parseJSON(getvalueCHART().responseText);
-		   // console.log(data1);
+                           async: false
+                       });
+                   }
+                   var chartData1 = generateChartData1();
 
-			var res = [];
-			for (var i = 1; i < data1.length; ++i){
-				
-				var date = data1[i].date;
-				res.push({'time':date,'value': parseFloat(data1[i].close).toFixed(2)});
-                var code1 = data1[i].code;
-                //var chartcode = data1[i].chartcode;
-				if (i==(data1.length-1)) { last_new  = data1[i].close;  time_new = data1[i].date}
-			}
-             $(".get_title_category2").append(code1 + ' (INTRADAY)');
+                   var chart = AmCharts.makeChart("chartdiv_customise1", {
+                       "type": "serial",
+                       "theme": "none",
+                       "marginRight": 40,
+                       "marginLeft": 70,
+                       "autoMarginOffset": 20,
+                       "dataProvider": chartData1,
+                       "valueAxes": [ {
+                           "id": "v1",
+                           "axisAlpha": 0,
+                           "position": "left",
+                           "ignoreAxisWidth": true
+                       } ],
+                       "balloon": {
+                           "borderThickness": 1,
+                           "shadowAlpha": 0
+                       },
+                       "graphs": [ {
+                           "id": "g1",
+                           "balloon": {
+                               "drop": true,
+                               "adjustBorderColor": false,
+                               "color": "#ffffff",
+                               "type": "smoothedLine"
+                           },
+                           "fillAlphas": 0.2,
+                           "bullet": "round",
+                           "bulletBorderAlpha": 1,
+                           "bulletColor": "#FFFFFF",
+                           "bulletSize": 5,
+                           "hideBulletsCount": 50,
+                           "lineThickness": 2,
+                           "title": "red line",
+                           "useLineColorForBulletBorder": true,
+                           "valueField": "close",
+                           "balloonText": "<span style='font-size:18px;'>[[value]]</span>"
+                       } ],
+                       "chartScrollbar": {
+                           "graph": "g1",
+                           "oppositeAxis":true,
+                           "offset":30,
+                           "scrollbarHeight": 30,
+                           "backgroundAlpha": 0,
+                           "selectedBackgroundAlpha": 0.1,
+                           "selectedBackgroundColor": "#888888",
+                           "graphFillAlpha": 0,
+                           "graphLineAlpha": 0.5,
+                           "selectedGraphFillAlpha": 0,
+                           "selectedGraphLineAlpha": 1,
+                           "autoGridCount":true,
+                           "color":"#AAAAAA"
+                       },
+                       "chartCursor": {
+                           "valueLineEnabled": true,
+                           "valueLineBalloonEnabled": true,
+                           "cursorAlpha": 0,
+                           "zoomable": false,
+                           "valueZoomable": true,
+                           "valueLineAlpha": 0.5,
+                           "categoryBalloonDateFormat": "JJ:NN:SS, DD MMMM",
+                           "cursorPosition": "mouse"
+                       },
+                       "valueScrollbar": {
+                           "autoGridCount": true,
+                           "color": "#000000",
+                           "scrollbarHeight": 50
+                       },
+                       "categoryField": "date",
+                       "categoryAxis": {
+                           "minPeriod": "mm",
+                           "parseDates": true,
+                           "dashLength": 1,
+                           "minorGridEnabled": true
+                       }
+                   });
 
-			function showChartTooltip(x, y, xValue, yValue) {
-				$('<div id="tooltip" class="chart-tooltip">' + yValue + '<\/div>').css({
-					position: 'absolute',
-					display: 'none',
-					top: y - 40,
-					left: x - 40,
-					border: '0px solid #ccc',
-					padding: '2px 6px',
-					'background-color': '#fff'
-				}).appendTo("body").fadeIn(200);
-			}
-			if ($('#chartdiv_customise1').size() != 0) {
-				
+                   chart.addListener("dataUpdated", zoomChart1);
+                   zoomChart1();
+                   function zoomChart1() {
+                       //chart.zoomToIndexes(chartData.length - 250, chartData.length - 100);
+                   }
+                   function generateChartData1() {
+                       var chartData1 = [];
 
-                    $('#site_statistics_loading').hide();
-                    $('#site_statistics_content').show();
+                       var data1 = jQuery.parseJSON(getvalueCHART_1().responseText);
+                       var code1 = data1[0].code;
+                       var res = [];
+                       $(".get_title_category2").append(code1 + ' (INTRADAY)');
+                       for (var i = 1; i < data1.length; ++i){
 
-                    var chart = AmCharts.makeChart( "chartdiv_customise1", {
-                        "type": "serial",
-                        "theme": "none",
-                        "marginRight": 40,
-                        "marginLeft": 70,
-                        "autoMarginOffset": 20,
-                        "dataDateFormat": "YYYY-MM-DD",
-                        "valueAxes": [ {
-                            "id": "v1",
-                            "axisAlpha": 0,
-                            "position": "left",
-                            "ignoreAxisWidth": true
-                        } ],
-                        "balloon": {
-                            "borderThickness": 1,
-                            "shadowAlpha": 0,
-                        },
-                        "graphs": [ {
-                            "id": "g1",
-                            "balloon": {
-                                "drop": true,
-                                "adjustBorderColor": false,
-                                "color": "#ffffff",
-                                "type": "smoothedLine"
-                            },
-                            "fillAlphas": 0.2,
-                            "bullet": "round",
-                            "bulletBorderAlpha": 1,
-                            "bulletColor": "#FFFFFF",
-                            "bulletSize": 5,
-                            "hideBulletsCount": 50,
-                            "lineThickness": 2,
-                            "title": "red line",
-                            "useLineColorForBulletBorder": true,
-                            "valueField": "value",
-                            "balloonText": "<span style='font-size:18px;'>[[value]]</span>"
-                        } ],
-                        "chartScrollbar": {
-                            "graph": "g1",
-                            "oppositeAxis":true,
-                            "offset":30,
-                            "scrollbarHeight": 30,
-                            "backgroundAlpha": 0,
-                            "selectedBackgroundAlpha": 0.1,
-                            "selectedBackgroundColor": "#888888",
-                            "graphFillAlpha": 0,
-                            "graphLineAlpha": 0.5,
-                            "selectedGraphFillAlpha": 0,
-                            "selectedGraphLineAlpha": 1,
-                            "autoGridCount":true,
-                            "color":"#AAAAAA"
-                        },
-                        "chartCursor": {
-                            "valueLineEnabled": true,
-                            "valueLineBalloonEnabled": true,
-                            "cursorAlpha": 0,
-                            "zoomable": false,
-                            "valueZoomable": true,
-                            "valueLineAlpha": 0.5
-                        },
-                        "valueScrollbar": {
-                            "autoGridCount": true,
-                            "color": "#000000",
-                            "scrollbarHeight": 50
-                        },
-                        "categoryField": "time",
-                        "categoryAxis": {
-                            "parseDates": true,
-                            "dashLength": 1,
-                            "minorGridEnabled": true,
-                            "labelColorField": "color",
-                            /*"labelFunction": function(valueText, serialDataItem, categoryAxis) {
-                                var p_type = valueText.substring(valueText.length - 10, 0);
-                                var p_date = valueText.substring(valueText.length - 10);
-                                valueText = p_type + "\n" + p_date;
-                                return valueText;
-                            }*/
-                        },
-                        "export": {
-                            "enabled": true
-                        },
-                        "dataProvider": res
-                    } );
-                        
-                        chart.addListener("rendered", zoomChart1);
-                        
-                        zoomChart1();
-                        
-                        
-                
-                        $('#chartdiv').closest('.portlet').find('.fullscreen').click(function() {
-                            chart.invalidateSize();
-                        });
-                        
-                    
-			};
-				function zoomChart1() {
-				
-				}
+                           var date = data1[i].date;
+                           chartData1.push({'date':date,'close': parseFloat(data1[i].close).toFixed(2)});
+
+
+                       }
+                       return chartData1;
+                   }
+                   //end chart 1
 				//char 2
 				
 				function getvalueCHART_2(){
@@ -169,7 +138,7 @@ define([
 						async: false
 					});
 				}
-                   var chartData = generateChartData();
+                   var chartData2 = generateChartData2();
 
                    var chart = AmCharts.makeChart("chartdiv_customise2", {
                        "type": "serial",
@@ -177,7 +146,7 @@ define([
                        "marginRight": 40,
                        "marginLeft": 70,
                        "autoMarginOffset": 20,
-                       "dataProvider": chartData,
+                       "dataProvider": chartData2,
                        "valueAxes": [ {
                            "id": "v1",
                            "axisAlpha": 0,
@@ -252,8 +221,8 @@ define([
                    function zoomChart2() {
                        //chart.zoomToIndexes(chartData.length - 250, chartData.length - 100);
                    }
-                   function generateChartData() {
-                       var chartData = [];
+                   function generateChartData2() {
+                       var chartData2 = [];
 
                        var data2 = jQuery.parseJSON(getvalueCHART_2().responseText);
                        var code2 = data2[0].code;
@@ -262,14 +231,14 @@ define([
                        for (var i = 1; i < data2.length; ++i){
 
                            var date = data2[i].date;
-                           chartData.push({'date':date,'close': parseFloat(data2[i].close).toFixed(2)});
+                           chartData2.push({'date':date,'close': parseFloat(data2[i].close).toFixed(2)});
 
 
                        }
-                       return chartData;
+                       return chartData2;
                    }
 				
-
+                    // end chart 2
 					
                });
             },
