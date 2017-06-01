3ee7c5737a2841b20e3bca51e8eb8ccd8de7446c
ifrcxuanhung <Xuanhung2211>
2017-06-01 16:04:12 +0700

    Merge branch 'master' of commo.ifrc.vn:~/git/commo
    # Conflicts:
    #	_/application/config/database.php

--- _/modules/welcome/views/market.php
+++ _/modules/welcome/views/market.php
@@ -2,15 +2,15 @@
 <!-- block box -->
 <?php echo $col1_market;?>
 
-<div class="col-md-4">
+<!--<div class="col-md-4">
     <div class="col-md-12">
         <div class="col-md-12 blocks use_fullscreen">
 
             <div class="portlet box blocks cus_hung" style="position:relative;">
                 <div class="portlet-title" style="background:#000099 !important;">
-                    <div class="caption"><?php echo $chartcode ?> (INTRADAY)</div>
+                    <div class="caption"><?php /*echo $chartcode */?> (INTRADAY)</div>
                     <div class="tools">
-                        <!-- <a href="" class="fullscreen"> </a>-->
+
                         <i class="fa fa-toggle-up fa-lg fullscreens"></i>
                     </div>
                 </div>
@@ -21,33 +21,18 @@
 
         </div>
 
-        <!--<div class="col-md-12 blocks use_fullscreen">
-            <div class="portlet box blocks cus_hung" style="position:relative;">
-                <div class="portlet-title" style="background:#000099 !important;">
-                    <div class="caption">
-                        <i class="fa"></i>TOP 4</div>
-                    <div class="tools">
-                        <i class="fa fa-toggle-up fa-lg fullscreens"></i>
-                    </div>
-                </div>
-                <div class="portlet-body background_portlet" style="margin-right:5px;">
 
-
-                    <div id="chartdiv4" class="chart" style="height: 270px; width: 110%; margin:-4%;"> </div>
-                </div>
-            </div>
-        </div>-->
     </div>
     <div class="col-md-12">
         <div class="col-md-12 blocks use_fullscreen">
 
             <div class="portlet box blocks cus_hung" style="position:relative;">
                 <div class="portlet-title" style="background:#000099 !important;">
-                    <div class="caption"><?php echo $chartcode ?> (HISTORY)</div>
+                    <div class="caption"><?php /*echo $chartcode */?> (HISTORY)</div>
                     <div class="tools">
-                        <!-- <a href="" class="fullscreen"> </a>-->
+
                         <i class="fa fa-toggle-up fa-lg fullscreens"></i>
-                        <!--<i class="fa fa-compress minscreens"></i>-->
+
                     </div>
                 </div>
                 <div class="portlet-body background_portlet" style="margin-right:5px;">
@@ -57,23 +42,8 @@
             </div>
 
         </div>
-        <!--<div class="col-md-12 blocks use_fullscreen">
-            <div class="portlet box blocks cus_hung" style="position:relative;">
-                <div class="portlet-title" style="background:#000099 !important;">
-                    <div class="caption">
-                        <i class="fa"></i>TOP 3</div>
-                    <div class="tools">
-                        <i class="fa fa-toggle-up fa-lg fullscreens"></i>
-                    </div>
-                </div>
-                <div class="portlet-body background_portlet" style="margin-right:5px;">
 
-
-                    <div id="chartdiv3" class="chart" style="height: 270px; width: 110%; margin:-4%;"> </div>
-                </div>
-            </div>
-         </div>-->
     </div>
 
 </div>
-<input type="hidden" id="chartcode" value="<?php echo $chartcode ?>"  />
+<input type="hidden" id="chartcode" value="<?php /*echo $chartcode */?>"  />-->
